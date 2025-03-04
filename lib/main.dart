3f3920ad5905be7cf0dd1e1f0724e40a09a94e19
Sefen-Mahrous <sefenma7rous@gmail.com>
2025-03-04 14:04:13 +0200

    4/3/2025

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes/views/home_page_view.dart';
 
 void main() {
   runApp(const NotesApp());
@@ -9,6 +10,10 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {HomePageView.id: (context) => HomePageView()},
+      initialRoute: HomePageView.id,
+    );
   }
 }
---
+++ lib/views/home_page_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatelessWidget {
+  static String id = 'HomePageView';
+  const HomePageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
